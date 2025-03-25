d78de3dc50833fab832c5b44b49131b1ded89589
PrathmeshJoshi-20 <alternate.joshi@gmail.com>
2025-03-26 01:22:46 +0530

    added new billing service and dockerfile

--- patient-service/src/test/java/com/pm/patientservice/PatientServiceApplicationTests.java
+++ patient-service/src/test/java/com/pm/patientservice/PatientServiceApplicationTests.java
@@ -2,8 +2,13 @@ package com.pm.patientservice;
 
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class PatientServiceApplicationTests {
 
